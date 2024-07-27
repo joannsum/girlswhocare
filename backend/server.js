ec91ea73b46390fdb06e0a98f627a7c122b274da
jjosum <joannsmsum@gmail.com>
2024-07-27 00:28:52 -0700

    setting up react and node

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Girl Who Share API');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
